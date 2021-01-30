bad8534e505ba604d89914be054ecf39e6915b17
Samvel Abrahamyan <samvel1024@gmail.com>
2021-01-30 14:55:54 +0100

    Reformat

--- src/components/SelectionInspector.tsx
+++ src/components/SelectionInspector.tsx
@@ -4,47 +4,47 @@
 
 import * as React from 'react';
 
-import { InspectorRow } from './InspectorRow';
+import {InspectorRow} from './InspectorRow';
 
 import './Inspector.css';
 
 interface SelectionInspectorProps {
-  selectedData: any;
-  onInputChange: (id: string, value: string, isBlur: boolean) => void;
+    selectedData: any;
+    onInputChange: (id: string, value: string, isBlur: boolean) => void;
 }
 
 export class SelectionInspector extends React.PureComponent<SelectionInspectorProps, {}> {
-  /**
-   * Render the object data, passing down property keys and values.
-   */
-  private renderObjectDetails() {
-    const selObj = this.props.selectedData;
-    const dets = [];
-    for (const k in selObj) {
-      const val = selObj[k];
-      const row = <InspectorRow
-                    key={k}
-                    id={k}
-                    value={val}
-                    onInputChange={this.props.onInputChange} />;
-      if (k === 'key') {
-        dets.unshift(row); // key always at start
-      } else {
-        dets.push(row);
-      }
+    public render() {
+        return (
+            <div id='myInspectorDiv' className='inspector'>
+                <table>
+                    <tbody>
+                    {this.renderObjectDetails()}
+                    </tbody>
+                </table>
+            </div>
+        );
     }
-    return dets;
-  }
 
-  public render() {
-    return (
-      <div id='myInspectorDiv' className='inspector'>
-        <table>
-          <tbody>
-            {this.renderObjectDetails()}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+    /**
+     * Render the object data, passing down property keys and values.
+     */
+    private renderObjectDetails() {
+        const selObj = this.props.selectedData;
+        const dets = [];
+        for (const k in selObj) {
+            const val = selObj[k];
+            const row = <InspectorRow
+                key={k}
+                id={k}
+                value={val}
+                onInputChange={this.props.onInputChange}/>;
+            if (k === 'key') {
+                dets.unshift(row); // key always at start
+            } else {
+                dets.push(row);
+            }
+        }
+        return dets;
+    }
 }
